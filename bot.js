59f581dc958698cb9cb6ceb9fcd8b1c56e0cac42
Robert Askam@ <rbaskam@gmail.com>
2019-06-25 09:10:19 +0100

    TEST New Key

--- bot.js
+++ bot.js
@@ -1,14 +1,15 @@
 const express = require('./node_modules/express');
 const app = express();
 const path = require('./node_modules/path');
-app.set('port', (process.env.PORT || 49167));
+app.set('port', (process.env.PORT || 5000));
 app.get('/', function (request, response) {
     response.sendFile(path.join(__dirname + '/index.html'));
 }).listen(app.get('port'), function () {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
 
-
+console.log('Consumer Key: ' + process.env.consumer_key);
+console.log('Consumer Key ENV: ' + ENV['consumer_key']);
 const Twit = require('./node_modules/twit');
 const TH = new Twit({ // Twit Handler
     consumer_key : process.env.consumer_key,
